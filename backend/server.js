24c8f9539afeb44d2de7b3f8cfad747b4b2c4b38
babulalmahto <babulalmatho4394@gmail.com>
2024-10-26 12:49:38 +0530

    backend folder added & server started

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+
+// app config
+const app=express()
+const port=process.env.PORT || 4080
+const localhost=process.env.localHost || '127.0.0.8'
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// api endpoints
+
+app.get('/',(req,res)=>{
+    res.send("API WORKING")
+})
+app.listen(port,()=>{
+    console.log(`Server started at http://${localhost}:${port}`)
+})
