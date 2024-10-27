1766f29fc6af7aa5437e233edfd631dcd27b488d
babulalmahto <babulalmatho4394@gmail.com>
2024-10-27 16:36:52 +0530

    creted UI part of Admin & Doctor Login as well token in localStorage, react toast added

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -14,7 +14,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary':'#5F6FFF'
+      }
+    },
   },
   plugins: [],
 }
