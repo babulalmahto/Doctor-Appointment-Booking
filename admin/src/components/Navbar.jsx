b2a8c48e62ff7fc94ba077277e021a10f90cf6f1
babulalmahto <babulalmatho4394@gmail.com>
2024-10-27 17:54:29 +0530

    Created Dashboard for Admin

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { AdminContext } from '../context/AdminContext'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
 
     const { aToken, setAToken } = useContext(AdminContext)
 
+    const navigate = useNavigate()
+
     const logout = () => {
+        navigate('/')
         aToken && setAToken('')
         aToken && localStorage.removeItem('aToken')
     }
