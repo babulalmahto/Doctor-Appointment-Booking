3394850044f9dd08cc3f2cc5c2d7d861dab90733
babulalmahto <babulalmatho4394@gmail.com>
2024-10-31 21:58:16 +0530

    Created API for Update user details for my profile

--- back-end/routes/userRoute.js
+++ back-end/routes/userRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import { getProfile, loginUser, registerUser } from '../controllers/userController.js'
+import { getProfile, loginUser, registerUser, updateProfile } from '../controllers/userController.js'
 import authUser from '../middlewares/authUser.js'
+import upload from '../middlewares/multer.js'
 
 const userRouter = express.Router()
 
@@ -8,5 +9,7 @@ userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
 
 userRouter.get('/get-profile', authUser, getProfile)
+//here using 2-middleware i)passing the form data ii)Authenticate the user and getting the usetId
+userRouter.post('/update-profile', upload.single('image'), authUser, updateProfile)
 
 export default userRouter
