ef5484d85e488b81468a300311f6abdbefbc9772
babulalmahto <babulalmatho4394@gmail.com>
2024-10-31 22:02:31 +0530

    Integrated API for fetching & updating user details for user my profile

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -12,7 +12,7 @@ const AppContextProvider = (props) => {
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
-
+    const [userData, setUserData] = useState(false)
 
     const getDoctorsData = async () => {
         try {
@@ -30,16 +30,43 @@ const AppContextProvider = (props) => {
         }
     }
 
+    // Update user profile
+    const loadUserProfileData = async () => {
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/user/get-profile', { headers: { token } })
+            if (data.success) {
+                setUserData(data.userData)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            res.json({ success: false, message: error.message })
+        }
+    }
+
     const value = {
         doctors,
         currencySymbol,
         token, setToken,
-        backendUrl
+        backendUrl,
+        userData, setUserData,
+        loadUserProfileData
     }
 
     useEffect(() => {
         getDoctorsData()
     }, [])
+
+    useEffect(() => {
+        if (token) {
+            loadUserProfileData()
+        } else {
+            setUserData(false)
+        }
+    }, [token])
 
     return (
         <AppContext.Provider value={value}>
