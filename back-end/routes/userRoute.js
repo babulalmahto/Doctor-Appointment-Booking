b077273bed4d0d120c5ed7f47a71d17916f90696
babulalmahto <babulalmatho4394@gmail.com>
2024-11-02 23:42:59 +0530

    Created API to book appointment

--- back-end/routes/userRoute.js
+++ back-end/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getProfile, loginUser, registerUser, updateProfile } from '../controllers/userController.js'
+import { bookAppointment, getProfile, loginUser, registerUser, updateProfile } from '../controllers/userController.js'
 import authUser from '../middlewares/authUser.js'
 import upload from '../middlewares/multer.js'
 
@@ -11,5 +11,8 @@ userRouter.post('/login', loginUser)
 userRouter.get('/get-profile', authUser, getProfile)
 //here using 2-middleware i)passing the form data ii)Authenticate the user and getting the usetId
 userRouter.post('/update-profile', upload.single('image'), authUser, updateProfile)
+
+// book appointment router
+userRouter.post('/book-appointment', authUser, bookAppointment)
 
 export default userRouter
